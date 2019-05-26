32e256848995cc71e5764c3635d4dce8919bf5ff
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-26 23:42:59 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(n)
+  arr = ''
+
+  if (!(n % 3 == 0) && !(n % 5 == 0))
+    return nil
+  end
+
+  if (n % 3 == 0)
+    arr += 'Fizz'
+  end
+
+  if (n % 5 == 0)
+    arr += 'Buzz'
+  end
+
+  arr
+end
