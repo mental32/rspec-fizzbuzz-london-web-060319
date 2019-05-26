b4a4c2dc8acfff1a1f041367df62dd87f33bd29f
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-26 23:35:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(max)
+  arr = ''
+  
+  if ((n % 3 == 0) && (n % 5 == 0))
+    arr << 'FizzBuzz'
+  elsif (n % 3 == 0)
+    arr << 'Fizz'
+  elsif (n % 5 == 0)
+    arr << 'Buzz'
+  else
+    arr << n
+  end
+
+  arr
+end
